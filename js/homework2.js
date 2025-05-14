bec7c402f1c75b499c19b1d18205f2b2d147aa30
VladLavrinec <vladlavrinec2010@gmail.com>
2025-05-14 22:14:36 +0300

    homework 2 (frontend3)

---
+++ js/homework2.js
@@ -0,0 +1,52 @@
+// 1
+let celsius = -50;
+let fahrenheit = 9 / 5 * celsius + 32;
+console.log(fahrenheit);
+
+// 2
+let daysInMonth = 30;
+let hoursInMonth = daysInMonth * 24;
+let minutesInMonth  = hoursInMonth * 60;
+console.log(minutesInMonth);
+
+// 3
+let health = 150;
+let healthLoss = health - 13 * 27;
+console.log(healthLoss);
+
+let energy = 100;
+let energyLoss = energy - 50;
+console.log(energyLoss);
+
+// 4
+let totalPrice = 100;
+let discount = 0.10;
+let discountedPrice = totalPrice * (1 - discount);
+console.log(discountedPrice);
+
+// 5
+const floatNumber = 12.78;
+const roundedDown = Math.floor(floatNumber);
+console.log(roundedDown);
+
+// 6 
+const floatString = "45.67";
+const parsedFloat = parseFloat(floatString);
+console.log(parsedFloat);
+
+// 7
+const intString = "123";
+const parsedInt = parseInt(intString);
+console.log(parsedInt);
+
+// 8
+let number = Math.sqrt(100);
+
+// 9
+const integer = 42;
+const stringNumber = "256";
+const convertedInt = parseInt(stringNumber);
+console.log(typeof convertedInt);
+const convertedString = integer.toString();;
+console.log(typeof convertedString);
+
